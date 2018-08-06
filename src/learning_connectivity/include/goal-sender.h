3f594487e9287b646feb328fa0b72eafd3623cd1
ComradePayne <dazogizzit@gmail.com>
2018-08-06 15:14:58 -0400

    Final push - I don't remember what I changed.

--- src/learning_connectivity/include/goal-sender.h
+++ src/learning_connectivity/include/goal-sender.h
@@ -5,9 +5,18 @@
 #include <string>
 #include <boost/tokenizer.hpp>
 #include <cstdlib>
+#include <math>
 
+//Reads points from csv file into a vector of goals.
 void read_trajectory(std::string);
+
+//Helper function to construct a goal message object, given x and y coordinates.
 move_base_msgs::MoveBaseGoal makeGoalMessage(double x, double y);
 void PIDController(std::vector<double> des_xy_coords, std::vector<double> parameters,
                     std::vector<double> u );
+                    
+//Allows user to specify a custom trajectory on the command line, if no file is supplied. 
 void getCustomTrajectory();
+
+//Given a current goal and the next goal, this function is intended to help calculate the angle towards the next goal in the goal list.
+double calculateAngleToNextGoal(move_base_msgs::MoveBaseGoal next_goal, move_base_msgs::MoveBaseGoal current_goal);
